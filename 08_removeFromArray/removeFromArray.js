0b184a714e39ae4bd8756b842a94d21c011dd284
brmart4 <brmart4@uwe.nsa.gov>
2026-02-06 13:01:37 -0500

    finished removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toBeRemoved) {
+    let resultsArr = [];
+
+    for (const i of arr) {
+        if (!toBeRemoved.includes(i)) {
+            resultsArr.push(i);
+        }
+    }
+    return resultsArr;
 };
 
 // Do not edit below this line
