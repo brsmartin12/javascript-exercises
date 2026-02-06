d642e7259cf5cb1b09e808fa0d4eefaa1b9ebf06
brmart4 <brmart4@uwe.nsa.gov>
2026-02-06 11:55:11 -0500

    finished the reverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArr = string.split('');
+    const reverseArr = stringArr.reverse();
+    const reverseString = reverseArr.join('');
+    return reverseString;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
