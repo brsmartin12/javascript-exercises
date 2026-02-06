bc44069bfa9abdb381e4a98aad8617c6a4eda12e
brmart4 <brmart4@uwe.nsa.gov>
2026-02-06 13:38:09 -0500

    finished the sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    // Reorganize inputs
+    if (last < first) {
+        let placeholder = first;
+        first = last;
+        last = placeholder;
+    }
 
+    if (first < 0 || last < 0) {
+        return 'ERROR';
+    }
+
+    if (!Number.isInteger(first) || !Number.isInteger(last)) {
+        return 'ERROR';
+    }
+
+    if (typeof first !== 'number' || typeof last !== 'number') {
+        return 'ERROR';
+    }
+    
+    let sum = 0;
+    for (let i=first; i <= last; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
