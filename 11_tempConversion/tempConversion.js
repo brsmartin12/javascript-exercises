f6f77ae11d483f393250dd6712ab479165048160
brmart4 <brmart4@uwe.nsa.gov>
2026-02-06 15:29:59 -0500

    finished tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let celsius = (fTemp - 32) * (5 / 9);
+  celsius = Math.round(10 * celsius) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fahrenheit = (cTemp * (9/5) + 32);
+  fahrenheit = Math.round(10 * fahrenheit) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
